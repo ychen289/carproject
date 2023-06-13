8d3b8b6cfec606c06b29899c4493f31ba8351cd4
YiFan Chen <ychen289@buffalo.edu>
2023-06-13 15:23:48 -0400

    fixed carCard not displaying properly problem

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
+const axios = require("axios");
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "b14f81e29dmsha75c6636af05199p1ad625jsnc142f566b7c4",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
-  const response = await fetch(
+
+  const response = await axios.get(
     "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
     { headers: headers }
   );
-  const result = await response.json();
-  return result;
+  return response.data; // Return the response data instead of the Promise object
 }
