6cb22890086eea97a21322b616813c78ac71b0ca
YiFan Chen <ychen289@buffalo.edu>
2023-06-14 01:09:48 -0400

    1h50m. stuck on modal backdrop not appearing.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,34 @@
 "use client";
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
+import { text } from "stream/consumers";
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
   btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={ btnType || "button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={() => {}}
     >
-      <span className={"flex-1"}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
